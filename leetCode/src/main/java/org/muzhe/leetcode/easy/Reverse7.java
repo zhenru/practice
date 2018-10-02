7955e1a25fc94ee75f1384326dc2bfb36e43284f
wanggagn11 <wanggang11@xiaomi.com>
2018-10-02 15:59:00 +0800

    添加新的反转数值的实现

--- leetCode/src/main/java/org/muzhe/leetcode/easy/Reverse7.java
+++ leetCode/src/main/java/org/muzhe/leetcode/easy/Reverse7.java
@@ -25,47 +25,82 @@ import java.util.List;
  * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
  */
 public class Reverse7 {
+    /**
+     * 这里的实现方法是当前的数值的每一位都记录下来，然后最后使用long类型来比较溢出的情况
+     *
+     * @param x
+     * @return
+     */
+//    public int reverse(int x) {
+//
+//        if (x == 0) {
+//            return 0;
+//        }
+//        boolean isNegative = x < 0;
+//        if (x < 0) {
+//            x = x * -1;
+//        }
+//
+//        List<Integer> counts = new ArrayList<Integer>();
+//        while (x > 0) {
+//            counts.add(x % 10);
+//            x = x / 10;
+//        }
+//        Long result = 0L;
+//        long base = 1L;
+//        boolean isStartZero = true;
+//        for (int i = counts.size() - 1; i >= 0; i--) {
+//            if (counts.get(i) == 0 && isStartZero) {
+//                continue;
+//            } else {
+//                long iResult = counts.get(i) * base;
+//                result += iResult;
+//                base *= 10;
+//                isStartZero = false;
+//            }
+//        }
+//
+//        if (isNegative) {
+//            result *= -1;
+//            if (result.compareTo(Long.valueOf(Integer.MIN_VALUE)) <0){
+//                return 0;
+//            }else {
+//                return result.intValue();
+//            }
+//        }
+//
+//        if (result.compareTo(Long.valueOf(Integer.MAX_VALUE)) >0){
+//            return 0;
+//        }
+//        return result.intValue();
+//    }
+
+    /**
+     * 将数值进行反转的方法类似与链表反转，可以将每一个都插入到链表头部，也可以将所有的值写到链表的尾部。
+     * @param x
+     * @return
+     */
     public int reverse(int x) {
 
         if (x == 0) {
             return 0;
         }
-        boolean isNegative = x < 0;
-        if (x < 0) {
-            x = x * -1;
-        }
-
-        List<Integer> counts = new ArrayList<Integer>();
-        while (x > 0) {
-            counts.add(x % 10);
-            x = x / 10;
-        }
-        Long result = 0L;
-        long base = 1L;
-        boolean isStartZero = true;
-        for (int i = counts.size() - 1; i >= 0; i--) {
-            if (counts.get(i) == 0 && isStartZero) {
-                continue;
-            } else {
-                long iResult = counts.get(i) * base;
-                result += iResult;
-                base *= 10;
-                isStartZero = false;
-            }
-        }
-
-        if (isNegative) {
-            result *= -1;
-            if (result.compareTo(Long.valueOf(Integer.MIN_VALUE)) <0){
-                return 0;
-            }else {
-                return result.intValue();
-            }
-        }
-
-        if (result.compareTo(Long.valueOf(Integer.MAX_VALUE)) >0){
+        if (x == Integer.MAX_VALUE || x == Integer.MIN_VALUE) {
             return 0;
         }
-        return result.intValue();
+        int sign = x > 0 ? 1 : -1;
+
+        x =Math.abs(x);
+        long result =0;
+        while (x!=0){
+
+            result = result*10 + x%10;
+            x  /=10;
+        }
+
+        if (result >Integer.MAX_VALUE){
+            return 0;
+        }
+        return sign * (int)result;
     }
 }
