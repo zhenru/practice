d3b5a69ec2c0228de2ef8f6773eeffdfb9334387
wanggagn11 <wanggang11@xiaomi.com>
2018-10-08 17:56:03 +0800

    添加测试用例

---
+++ leetCode/src/test/java/org/muzhe/dsa/tsinghua/tree/DefaultBinNodeTest.java
@@ -0,0 +1,55 @@
+package org.muzhe.dsa.tsinghua.tree;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author muzhe-wang on  18-10-8 下午5:55.
+ */
+public class DefaultBinNodeTest {
+
+    @Test
+    public void size() {
+    }
+
+    @Test
+    public void insertAsLeft() {
+    }
+
+    @Test
+    public void insertAsRight() {
+    }
+
+    @Test
+    public void succ() {
+    }
+
+    @Test
+    public void travLevel() {
+    }
+
+    @Test
+    public void travPre() {
+    }
+
+    @Test
+    public void travPost() {
+    }
+
+    @Test
+    public void low() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void zig() {
+    }
+
+    @Test
+    public void zag() {
+    }
+}
