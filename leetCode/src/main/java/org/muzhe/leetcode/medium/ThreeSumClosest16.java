8c3524908c6258b723b100b4fd2869d1a3f3816f
wanggagn11 <wanggang11@xiaomi.com>
2018-10-17 11:28:53 +0800

    添加需要完成的题目的实现

---
+++ leetCode/src/main/java/org/muzhe/leetcode/medium/ThreeSumClosest16.java
@@ -0,0 +1,19 @@
+package org.muzhe.leetcode.medium;
+
+/**
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，
+ * 使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * <p>
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.
+ * <p>
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).
+ *
+ * @author muzhe-wang on  18-10-17 上午11:22.
+ */
+public class ThreeSumClosest16 {
+    public int threeSumClosest(int[] nums, int target) {
+
+        //获取这个三个数的和　这个是１６题
+        return 0;
+    }
+}
